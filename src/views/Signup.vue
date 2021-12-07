620fa41db43ba50229976b6d8afc396a0f40993c
qbin-home <2538323140@qq.com>
2021-12-07 23:20:30 +0800

    feat: 注册用户

---
+++ src/views/Signup.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="signup-page mx-auto p-3 w-330">
+    <h5 class="my-4 text-center">注册者也用户</h5>
+    <validate-form @form-submit="onFormSubmit">
+      <div class="mb">
+        <label class="form-label">邮箱地址</label>
+        <validate-input
+          :rules="emailRules"
+          v-model="formData.email"
+          placeholder="请输入邮箱地址"
+          type="text"
+        />
+      </div>
+      <div class="mb">
+        <label class="form-label">昵称</label>
+        <validate-input
+          :rules="nameRules"
+          v-model="formData.nickName"
+          placeholder="请输入昵称"
+          type="text"
+        />
+      </div>
+      <div class="mb">
+        <label class="form-label">密码</label>
+        <validate-input
+          :rules="passwordRules"
+          v-model="formData.password"
+          placeholder="请输入密码"
+          type="password"
+        />
+      </div>
+      <div class="mb">
+        <label class="form-label">重复密码</label>
+        <validate-input
+          :rules="repeatPasswordRules"
+          v-model="formData.repeatPassword"
+          placeholder="请再次输入密码"
+          type="password"
+        />
+      </div>
+      <template #submit>
+        <button type="submit" class="btn btn-primary btn-block btn-large">注册新用户</button>
+      </template>
+    </validate-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from 'vue'
+import axios from 'axios'
+import { useRouter } from 'vue-router'
+import ValidateInput, { RulesProp } from '@/components/ValidateInput.vue'
+import ValidateForm from '@/components/ValidateForm.vue'
+import createMessage from '@/components/createMessage'
+
+export default defineComponent({
+  name: 'Component',
+  components: {
+    ValidateInput,
+    ValidateForm,
+  },
+  setup() {
+    const router = useRouter()
+    const formData = reactive({
+      email: '',
+      nickName: '',
+      password: '',
+      repeatPassword: '',
+    })
+    const emailRules: RulesProp = [
+      { type: 'required', message: '电子邮箱地址不能为空' },
+      { type: 'email', message: '请输入正确的电子邮箱格式' },
+    ]
+    const nameRules: RulesProp = [{ type: 'required', message: '昵称不能为空' }]
+    const passwordRules: RulesProp = [{ type: 'required', message: '密码不能为空' }]
+    const repeatPasswordRules: RulesProp = [
+      { type: 'required', message: '重复密码不能为空' },
+      {
+        type: 'custom',
+        validator: () => {
+          return formData.password === formData.repeatPassword
+        },
+        message: '两次输入的密码不一致',
+      },
+    ]
+
+    const onFormSubmit = (result: boolean) => {
+      if (result) {
+        const payload = {
+          email: formData.email,
+          password: formData.password,
+          nickName: formData.nickName,
+        }
+        axios
+          .post('/api/users/', payload)
+          .then((data) => {
+            createMessage('注册成功 正在跳转登录页面', 'success')
+            setTimeout(() => {
+              router.push('/login')
+            }, 2000)
+          })
+          .catch((e) => {
+            console.log(e)
+          })
+      }
+    }
+
+    return {
+      emailRules,
+      nameRules,
+      passwordRules,
+      repeatPasswordRules,
+      formData,
+      onFormSubmit,
+    }
+  },
+})
+</script>
+
+<style scoped></style>
